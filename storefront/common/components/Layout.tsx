a538cee00b48ac105ef85ec3337f05c336a3e44b
NPT-102 <nptho23@clc.fitus.edu.vn>
2026-05-16 03:21:16 +0700

    feat: change title

--- storefront/common/components/Layout.tsx
+++ storefront/common/components/Layout.tsx
@@ -12,7 +12,7 @@ export default function Layout({ children }: Props) {
   return (
     <>
       <Head>
-        <title>Yas - Storefront</title>
+        <title>Nguyen Phuc Tho - Storefront</title>
         <meta name="description" content="Yet another shop storefront" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
